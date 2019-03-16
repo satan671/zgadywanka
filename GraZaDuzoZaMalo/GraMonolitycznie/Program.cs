74538336694ee75e795a8a4dafbbafce92a9a5cc
Dominik Pyciak <dominik.pyciak@gmail.com>
2019-03-16 19:15:45 +0100

    Wersja 1

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -10,7 +10,11 @@ namespace GraMonolitycznie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("dzień dobry");
+            Console.WriteLine("Witaj! ");
+            Console.Write("Podaj swoję imie: ");
+            string x = Console.ReadLine();
+            Console.WriteLine($"Witaj {x}");
+
         }
     }
 }
