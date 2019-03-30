655a4751a6e2dd55d05733bb9bf8b6329c5f8efe
Dominik Pyciak <48623675+satan671@users.noreply.github.com>
2019-03-30 18:50:37 +0100

    Jeden ruch w grze

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -10,11 +10,42 @@ namespace GraMonolitycznie
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Witaj! ");
             Console.Write("Podaj swoję imie: ");
             string x = Console.ReadLine();
             Console.WriteLine($"Witaj {x}");
+            */
 
+            //1.komputer losuje liczbe
+            Random generator = new Random();
+            int wylosowana = generator.Next(1, 101);
+            Console.WriteLine("Wylosowałem liczbe od 1 do 100, \n Odgadnij ją" );
+#if(DEBUG)
+            Console.WriteLine(wylosowana);
+#endif
+
+            //2.czlowiek proponuje roziwazanie
+
+            Console.Write("Podaj swoją propozycję: ");
+            string tekst = Console.ReadLine();
+            int propozycja = Convert.ToInt32(tekst);
+            Console.WriteLine($"Przyjąłem wartość {propozycja}" );
+
+
+
+            //3.komputer ocenia propozycje,
+            if (propozycja<wylosowana)
+                Console.WriteLine("Za mało");
+            else if(propozycja > wylosowana)
+                Console.WriteLine("Za dużo");
+            else
+                Console.WriteLine("Trafiono");
+
+
+
+            Console.WriteLine("Koniec gry");
+            string a = Console.ReadLine();
         }
     }
 }
